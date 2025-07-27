9ae36a5c141b3c29c72226b12022cf49f9c37260
Soran <antien.huang@gmail.com>
2025-07-27 02:17:12 +0100

    Add main scene

--- scripts/Enemy.cs
+++ scripts/Enemy.cs
@@ -5,7 +5,7 @@ public partial class Enemy : RigidBody2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprited2D");
+        var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
         string[] enemyTypes = animatedSprite2D.SpriteFrames.GetAnimationNames();
         animatedSprite2D.Play(enemyTypes[GD.Randi() % enemyTypes.Length]);
 	}
---
+++ scripts/Main.cs
@@ -0,0 +1,65 @@
+using System.Diagnostics;
+using Godot;
+
+public partial class Main : Node
+{
+    [Export]
+    public PackedScene EnemyScene { get; set; }
+
+    private int _score;
+
+    public void GameOver()
+    {
+        GetNode<Timer>("EnemyTimer").Stop();
+        GetNode<Timer>("ScoreTimer").Stop();
+    }
+
+    public void NewGame()
+    {
+        _score = 0;
+
+        var player = GetNode<Player>("Player");
+        var startPosition = GetNode<Marker2D>("StartPosition");
+        player.Start(startPosition.Position);
+
+        GetNode<Timer>("StartTimer").Start();
+    }
+
+    private void OnStartTimerTimeout()
+    {
+        GetNode<Timer>("EnemyTimer").Start();
+        GetNode<Timer>("ScoreTimer").Start();
+    }
+
+    private void OnScoreTimerTimeout()
+    {
+        _score++;
+    }
+
+    private void OnEnemyTimerTimeout()
+    {
+        Enemy enemy = EnemyScene.Instantiate<Enemy>();
+
+        var enemySpawnLocation = GetNode<PathFollow2D>("EnemyPath/EnemySpawnLocation");
+        enemySpawnLocation.ProgressRatio = GD.Randf();
+        enemy.Position = enemySpawnLocation.Position;
+
+        // Set the enemy to move toward player
+        Player player = GetNode<Player>("Player");
+        Vector2 direction = (player.GlobalPosition - enemySpawnLocation.Position).Normalized();
+        float randomAngle = (float)GD.RandRange(-30.0f, 30.0f) * Mathf.Pi / 180.0f;
+        direction = direction.Rotated(randomAngle);
+        enemy.Rotate(direction.Angle());
+
+
+        // Choose the velocity
+        var velocity = new Vector2((float)GD.RandRange(350.0, 550.0), 0);
+        enemy.LinearVelocity = velocity.Rotated(direction.Angle());
+
+        AddChild(enemy);
+    }
+    public override void _Ready()
+    {
+        NewGame();
+    }
+}
